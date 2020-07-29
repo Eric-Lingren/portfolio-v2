5ac8fc2eb4c994ca04976c16fe02b1b8cac4b27c
Eric Lingren <ericlingren@gmail.com>
2020-07-28 18:54:07 -0600

    improving mobile styles

--- src/components/standard/work/WorkDetails.js
+++ src/components/standard/work/WorkDetails.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import { Carousel } from 'react-responsive-carousel'
-import Navbar from '../Navbar'
 import WorkDetailsNavbar from './WorkDetailsNav'
 import workData from './workData.json'
 
