55db71da9a28b5e116207aa6a8ef3afafedf2551
Eric Lingren <ericlingren@gmail.com>
2020-09-17 19:57:15 -0600

    moding footer mobile friendly

--- src/components/standard/Footer.js
+++ src/components/standard/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
 
             <h1 className='footer-text'>© {currentYear} </h1>
             <h1 className='footer-text'>Made with <span className='heart'>♥</span> by Eric Lingren</h1>
-            
+
             <div className='footer-contact-icon-wrapper'>
                 <a target='_blank' rel="noopener noreferrer" href='https://www.github.com/Eric-Lingren'>
                     <img className='footer-contact-icon-github' src={githubIcon} alt='github' />
